4008d02bdd3247305e502afbbfa60b171b27b119
dipankar-datta <dattapro5@gmail.com>
2019-09-22 21:58:25 +0530

    Added DB schema with data file.

--- others/myretail-main-schema-data.sql
+++ others/myretail-main-schema-data.sql
@@ -13,10 +13,12 @@
 
 
 -- Dumping database structure for myretail_main
+DROP DATABASE IF EXISTS `myretail_main`;
 CREATE DATABASE IF NOT EXISTS `myretail_main` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `myretail_main`;
 
 -- Dumping structure for table myretail_main.login
+DROP TABLE IF EXISTS `login`;
 CREATE TABLE IF NOT EXISTS `login` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(50) DEFAULT NULL,
@@ -29,7 +31,10 @@ CREATE TABLE IF NOT EXISTS `login` (
   KEY `FK_login_users` (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
 
--- Data exporting was unselected.
+-- Dumping data for table myretail_main.login: ~0 rows (approximately)
+DELETE FROM `login`;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
